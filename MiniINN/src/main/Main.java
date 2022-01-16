00e8f68cf20c1bcce6882938f78e1e74eb9f1dea
b0b4u <92981021+b0b4u@users.noreply.github.com>
2022-01-16 12:56:36 +0200

    Update Main.java

--- MiniINN/src/main/Main.java
+++ MiniINN/src/main/Main.java
@@ -18,6 +18,23 @@ public class Main extends Application {
     public static void main(String[] args) {
 
         launch(args);
+        
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Mini INN");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+
 
     }
 
